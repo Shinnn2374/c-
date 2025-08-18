5c59542f2f1d12746c541cda7fe5379790e69520
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-08-18 20:50:09 +0300

    ...

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,3 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello c#");
+﻿byte number = 4;
+Console.WriteLine(number);
--- dotnet/obj/Debug/net9.0/dotnet.AssemblyInfo.cs
+++ dotnet/obj/Debug/net9.0/dotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+871ff64465942363c583a9a197765a7fd72d4dc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e32c46137c595e10532bfd8c28d3eb168768bb38")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
